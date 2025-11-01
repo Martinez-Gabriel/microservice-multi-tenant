6afd396569d57686e2e10f4836d14c1395f161ca
Gabriel Martinez <mart.gabriel96@gmail.com>
2025-11-01 05:01:40 -0300

    fix(tenants.controller.ts): change path on import dto

--- src/tenants/tenants.controller.ts
+++ src/tenants/tenants.controller.ts
@@ -8,8 +8,8 @@ import {
   Put,
 } from '@nestjs/common';
 import { TenantsService } from './tenants.service';
-import { CreateTenantDto } from './dto/create-dto';
-import { UpdateTenantDto } from './dto/update-dto';
+import { CreateTenantDto } from './dto/create-tenant-dto';
+import { UpdateTenantDto } from './dto/update-tenant-dto';
 
 @Controller('tenants')
 export class TenantsController {
